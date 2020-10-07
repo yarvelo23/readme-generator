0fa5391b53d5fce5d2f69b9e6d94987717891dd4
Yerson Arvelo <yerson.a06@gmail.com>
2020-10-06 23:30:28 -0400

    finished question array

--- index.js
+++ index.js
@@ -16,8 +16,50 @@ const questions = [
         message: "What is your project title?"
     },
     {
-        
-    }
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please provide the project license or your badge link"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
 ];
 
 // function to write README file
