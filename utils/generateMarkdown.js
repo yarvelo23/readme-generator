bdb8e8f71daa83e5a8f91ec060d2a959008e3ddf
Yerson Arvelo <yerson.a06@gmail.com>
2020-10-07 21:40:16 -0400

    function to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,55 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # **${data.title}**
+
+  ${data.badge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of contents
+
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributors](#Contributors)
+  - [Test](#Test)
+  - [Repository Link](#Repository)
+  - [GitHub Info](#GitHub)
+
+  ## Installation
+  
+    ${data.installation}
+
+  ## Usage
+
+    ${data.usage}
+
+  ## License
+
+    ${data.license}
+  
+  ## Contributors
+
+    ${data.contributing}
+  
+  ## Test
+  
+    ${data.test}
+  
+  ## Repository
+
+  - [Project Repo](${data.repo})
+
+  ## GitHub
+
+  ![Image of me](${githubInfo.githubImage})
+    - ${githubInfo.name}
+    - [GitHub Profile](${githubInfo.profile})
+    - <${githubInfo.email}>
 
 `;
 }
